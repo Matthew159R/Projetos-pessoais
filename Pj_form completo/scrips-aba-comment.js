6ed57e5eebfcc3a264c914b9968da7989c75640e
Matthew159R <117731536+Matthew159R@users.noreply.github.com>
2023-03-17 19:38:37 -0300

    Pj_form completo
    Front end do pj form completo

---
+++ Pj_form completo/scrips-aba-comment.js
@@ -0,0 +1,44 @@
+const usernameInput = document.querySelector('.username')
+const buttonConfirmedUsername = document.querySelector('.enviar')
+const nome = document.querySelector('.nomeh4')
+const avatar = document.querySelector('.avatar')
+const foto = document.querySelector('.foto')
+const comentar = document.querySelector('.comentar')
+const comment = document.querySelector('.commentH4')
+const commentCamp = document.querySelector('textarea')
+
+buttonConfirmedUsername.addEventListener('click', event => {
+
+  //Faz o a o nome digitado aparecer na tela
+  event.preventDefault()
+  document.querySelector('.container').style.display = 'inline-block'
+  document.querySelector('.container3').style.display = 'none'
+  nome.textContent = usernameInput.value
+
+  //Fazer com que um comentario novo apareça embaixo do outro
+  
+
+})
+
+const avatarChildren = Array.from(avatar.children)
+
+  avatarChildren.forEach(element => {
+    element.addEventListener('click', event => {
+      const clickedElement = event.target
+      if(clickedElement === avatarChildren[0]){
+        document.querySelector('.fotoImg').src = 'Avatar1.png'
+      }else if(clickedElement === avatarChildren[1]){
+        document.querySelector('.fotoImg').src = 'Avatar2.png'
+      }
+    })
+  })
+
+comentar.addEventListener('click', event => {
+  event.preventDefault()
+  
+  comment.textContent = commentCamp.value
+  foto.style.display  = 'block'
+  commentCamp.value = ''
+})
+
+
